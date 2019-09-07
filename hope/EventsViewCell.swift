a65f1a9ff7d5e9208285c7147093d5baecbe9b06
yasinehsan <yasinehsan11@gmail.com>
2019-09-07 13:40:04 -0400

    added tvc

--- hope/EventsViewCell.swift
+++ hope/EventsViewCell.swift
@@ -9,23 +9,15 @@
 import UIKit
 
 class EventsViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
-    
     @IBOutlet weak var eventImageView: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var dayLabel: UILabel!
     
+    func setEvents(event: Event) {
+        eventImageView.image = event.image
+        dateLabel.text = event.date
+        dayLabel.text = event.day
+    }
     
     
     
--- hope/ProfileViewController.swift
+++ hope/ProfileViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    @IBOutlet weak var eventTableView: UITableView!
+    @IBOutlet weak var numEventsLabel: UILabel!
+    
+    var events: [Event] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        events = createArray()
+        eventTableView.delegate = self
+        eventTableView.dataSource = self
+    }
+    
+    func createArray() -> [Event] {
+        let tempBool: [Event] = [Event.e1, Event.e2, Event.e3, Event.e4]
+        return tempBool
         
     }
     
     
-    @IBOutlet weak var numEventsLabel: UILabel!
-    
+}
 
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
+    }
     
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let event = events[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EventsCell") as! EventsViewCell
+        
+        cell.setEvents(event: event)
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 }
