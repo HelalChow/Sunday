1c2e5bfc7c64a6d24d8f734ffe314320a22c3961
yasinehsan <yasinehsan11@gmail.com>
2019-09-07 06:48:37 -0400

    able to see current location

---
+++ hope/MapViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MapViewController.swift
+//  hope
+//
+//  Created by Yasin Ehsan on 9/7/19.
+//  Copyright © 2019 Yasin Ehsan. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userLocationSetup()
+    }
+    
+    func userLocationSetup(){
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization() //we can ask this later
+        locationManager.startUpdatingLocation()
+        locationManager.distanceFilter = 100
+        mapView.showsUserLocation = true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+}
