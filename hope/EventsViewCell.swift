beb6a011e0a6ebc4b02d0508f234c4a355b49446
hchowdhury3273 <hc2324@nyu.edu>
2019-09-07 08:01:59 -0400

    image profile

---
+++ hope/EventsViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  EventsViewCell.swift
+//  hope
+//
+//  Created by Helal Chowdhury on 9/7/19.
+//  Copyright © 2019 Yasin Ehsan. All rights reserved.
+//
+
+import UIKit
+
+class EventsViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    
+    @IBOutlet weak var eventImageView: UIImageView!
+    
+    
+    
+    
+    
+    
+}
